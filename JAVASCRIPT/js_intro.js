74c51e9ddc14578b3cfa36077b77d525686c9de5
unknown <indus.sharma211@gmail.com>
2026-02-24 10:20:58 +0530

    JS INTRO AND DEMO FILE

---
+++ JAVASCRIPT/js_intro.js
@@ -0,0 +1,11 @@
+/* 1.JAVASCRIPT IS A CROSS PLATFORM OBJECT BASED SCRIPTING LANGUAGE FOR CLIENT AND SERVER APPLICATION.
+2.loosely typed language .
+3. high level and multi-paradigm.
+4.intrepreted and just-in-time compilation.
+5.extends functionality of web page .
+6.written in html document.
+7.reacts to user action.
+
+*/
+
+/*
